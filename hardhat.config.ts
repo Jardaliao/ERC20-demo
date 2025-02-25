4cd4071c3149169ea746c169e86edf25e171314c
Jay Liao <jardaliao@gmail.com>
2025-02-25 14:25:26 +0800

    feat: deploy to testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config({path: __dirname + '/.env'});
+
+const { ALCHEMY_API_KEY, MNEMONIC } = process.env;
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: { mnemonic: MNEMONIC },
+    },
+  },
 };
 
 export default config;

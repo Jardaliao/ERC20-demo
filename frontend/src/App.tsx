2f23120ed3eb850ef5827093d774f9667e4e24b8
Jarda <jardaliao@gmail.com>
2025-03-01 20:04:46 +0800

    chore: refactor to ts + vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [balance, setBalance] = useState(0);
+
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        dsfs
+      </div>
+    </>
+  )
+}
+
+export default App
